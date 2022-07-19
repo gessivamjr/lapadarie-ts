8e169df3feaf4d6f1e1939f73b5996a9ea1e7bb6
Gessivam Júnior <juniorgessivam@gmail.com>
2022-07-18 22:01:16 -0300

    feat: add morgan and POO to App

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,44 @@
-import express from "express";
+import express, { Application } from "express";
+import morgan from "morgan";
 
-const app = express();
+export class App {
+  private app: Application;
+  private port = process.env.PORT || 3000;
 
-export default app;
+  constructor() {
+    this.app = express();
+    this.listen();
+    this.middlewares();
+    this.cors();
+    this.morgan();
+  }
+
+  private listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+
+  private middlewares(): void {
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json());
+  }
+
+  private cors(): void {
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+      if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+        return res.status(200).send({});
+      }
+
+      next();
+    });
+  }
+
+  private morgan(): void {
+    this.app.use(morgan("dev"));
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
-import http from "http";
-import app from ".";
+import { App } from ".";
 
-const port = 3000;
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+new App();
